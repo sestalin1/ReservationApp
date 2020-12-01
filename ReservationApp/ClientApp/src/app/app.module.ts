c9ed7a599841676da2472a2d0659b715fab8856d
Juan Leonardo <crjleonardo5@gmail.com>
2020-12-01 13:55:35 -0400

    Angular Components generated: Contacts ContactTypes Reservations

--- ReservationApp/ClientApp/src/app/app.module.ts
+++ ReservationApp/ClientApp/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactTypesComponent } from './contact-types/contact-types.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ReservationsComponent } from './reservations/reservations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactTypesComponent,
+    ContactsComponent,
+    ReservationsComponent
   ],
   imports: [
     BrowserModule
---
+++ ReservationApp/ClientApp/src/app/contact-types/contact-types.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContactTypesComponent } from './contact-types.component';
+
+describe('ContactTypesComponent', () => {
+  let component: ContactTypesComponent;
+  let fixture: ComponentFixture<ContactTypesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ContactTypesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContactTypesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
